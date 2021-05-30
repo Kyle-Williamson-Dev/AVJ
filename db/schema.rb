278badc94534284f8e14c51fbae74426834c96e8
Kyle Williamson <kwilliamson0232@gmail.com>
2021-05-30 14:02:50 -0600

    Creating side nav

--- db/schema.rb
+++ db/schema.rb
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 0) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-end
-=======
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -48,4 +28,3 @@ ActiveRecord::Schema.define(version: 2021_05_27_030902) do
   end
 
 end
->>>>>>> 611fa8b48796e3d0f6d0d9a74c76c988c70f5f98
